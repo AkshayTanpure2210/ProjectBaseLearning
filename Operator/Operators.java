34b5900c0e33491662e6d5bb051cd20ab5bc5ec9
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2022-12-01 13:36:58 +0530

    my commit

---
+++ Operator/Operators.java
@@ -0,0 +1,265 @@
+
+package com.stringInfo.Operator;
+
+public class Operators {
+/*public static void main(String[] args) {
+    
+
+    // Arthmatic Operator 
+
+    int a=10;
+    int b =5;
+
+    int add = a+b;
+    System.out.println(add);
+    int substract =a-b;
+    System.out.println(substract);
+    int product = a*b;
+    System.out.println(product);
+    int product2 = a/b;
+    System.out.println(product2);
+
+     uninary operator 
+       i++,i--
+       ++ called increment -- called as decrement operator
+       increment and decrement have two type 
+       increment  ++i(preicrement) and i++(postincrement)
+       decrement operator also have predecremnt(--i) and postdecrement(i--) 
+
+ Example base on uninary and its type of operator.pre and post example 
+++i 
+
+    int x=20;
+    int y=0;
+    y =++x;
+    System.out.println(x);
+    System.out.println(y);
+
+
+// post operator
+    int x1=20;
+    int y1=0;  
+    y1=x1++;
+    System.out.println(x1);
+    System.out.println(y1);
+
+// RELATIONAL OPERATOR ()
+
+//== (equal to)
+    int aa=78;
+    int bb=45;
+    System.out.println("if equal thn true if not equal than false ");
+    boolean bn= bb==aa;
+    System.out.println("\n"+ bn);
+
+    System.out.println("\n");
+
+if(aa==bb)
+    {
+    System.out.println("match");  
+    }
+else{
+    System.out.println("unmatch"); 
+    }
+
+//  != not equal to 
+
+     int a1=65;
+     int b1=20;
+     System.out.println("print true or false");
+     boolean bk = a1!=b1;
+     System.out.println(bk +"\n");
+//greater than and less than 
+   if (aa>bb)
+    {
+       System.out.println(" a1 is greater than b1");
+    }
+   else 
+   {
+      System.out.println("a1 is less than b1");
+   }
+   boolean bx = (aa>bb);
+   System.out.println(bx);
+   System.out.println("\n");
+
+//logical Operator 
+// &&  logical AND 
+// ||  logical Or 
+//  !  Not
+
+//logical &&
+
+    System.out.println(" logical operator");
+    int o =10;
+    int p =15;
+    int q =25;
+// method 1
+    boolean op = (o<p && p<q);
+    System.out.println(op+":o is less than p and p is less than q thn true");
+    boolean po = (o>p && p>q);
+    System.out.println(po + ":if p is greater than o and q is greater p than  true ");
+
+//by using if else statement
+
+    if (o<p && p<q)
+      {
+        System.out.println(" statement true");
+      }
+    else
+     {
+        System.out.println("statement is false ");
+     }
+
+//logical operator ||
+
+int xyz= 10;
+int zyx= 25;
+
+boolean yzx = (xyz>zyx || xyz<zyx);
+System.out.println("xyz is gretter than zyx or xyz is less than zyx ");
+System.out.println(yzx);
+boolean xy=(xyz>zyx || xyz ==zyx);
+System.out.println("xyz is greater than zyx or xyz is equal to zyx" );
+System.out.println(xy);
+System.out.println("\n"+ " we can use Not operator !");
+System.out.println(!(xy));
+
+
+//  bitwise operator 
+
+ 
+&(binary AND operator )
+| (binary OR operator)
+ (binary XOR operator )
+ ~ convert 0 to 1 & 1 to 0
+
+ <<(binary left shift)
+ >>(binary right shift)
+ 
+// example 
+
+int r1= 0101;
+int s1= 1010;
+
+int w =(r1 & s1);
+System.out.println(w);
+System.out.println(((int)w));
+
+int q1=(r1 | s1);
+System.out.println(q1);
+
+// example  ~
+
+int bc =0101;
+int cd=~bc;
+System.out.println(cd);
+System.out.println((bc<<1));
+System.out.println((bc>>1));
+
+// Assignment Operator 
+int m=20;
+int n=30;
+int mn =m+n;
+int j = m+=n;
+System.out.println(mn);
+System.out.println(j);
+
+ m-=n
+ m*=n
+ m/=n
+ m%=n
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
---
+++ Strings/Strings.java
@@ -0,0 +1,167 @@
+package com.stringInfo.Operator;
+import java.util.*;
+import java.lang.String;
+
+public class Strings /*{
+public static void main(String[] args) {
+    
+{
+ // concatenation = tow diffrent string converted into one string 
+// example 1
+
+String name1 = "Akshay";
+
+String name2 ="Tanpure";
+
+
+String fullname = name1 + name2;
+System.out.println(fullname);
+System.out.println(fullname.length());   
+//  .length() we can use this method for masure length of string 
+
+//use charAt for every char value present in string
+
+for(int i =0; i <fullname.length(); i++)
+{   System.out.println();
+    System.out.println("next char :"+fullname.charAt(i));
+    
+}
+
+
+
+// compare 
+
+System.out.println("Example on CompareTo()");
+String team = "India";
+String team2= "india";
+String team3= "India";
+   if (team.compareTo(team2)==0)
+      {
+        System.out.println("string are equal");
+      }
+  else 
+      {
+        System.out.println("string are not equal");
+      }
+
+        System.out.println(" 2nd example ");
+
+   if (team.compareTo(team3)==0)
+     {
+        System.out.println("string are equal");
+     }
+  else 
+     {
+        System.out.println("string are not equal");
+     }
+
+
+//same problem in == 
+// why we consider compareTo method in String comparison and why we can not using == in String comparision
+
+if(new String("India")==new String("India"))
+  
+  System.out.println("teams name is equal ");
+
+   else 
+   {
+      System.out.println(" teams name is not equal");
+   }
+
+System.out.println(" from above result we can understand for String comparision CompareTo() is suitable");
+
+
+
+
+// Substring - if string is long / short and we want to specific part of that string that time we can use this substring concept
+
+
+String motivationnalQouets="A year from now you will wish you had started today.";
+
+String line =motivationnalQouets.substring(6, motivationnalQouets.length());
+String word = motivationnalQouets.substring(2,6);
+String line1 = motivationnalQouets.substring(1);
+System.out.println(line); // here we use starting index and .length method
+System.out.println("acess word : "+ word);
+System.out.println(line1); // here we give only starting index 
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+}                                                                               
+
+
